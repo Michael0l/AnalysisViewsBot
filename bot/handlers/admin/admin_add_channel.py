c0895d555b242ddafdf7b48bac4cf0773d052df0
Michael0l <40627547+Michael0l@users.noreply.github.com>
2023-01-16 21:59:31 +0300

    Update admin_add_channel.py

--- bot/handlers/admin/admin_add_channel.py
+++ bot/handlers/admin/admin_add_channel.py
@@ -6,8 +6,6 @@ from bot.states.states import AddChannel
 from bot.filters.admin_filter import IsAdmin
 
 
-
-
 @dp.message_handler(IsAdmin(), state=AddChannel.channel, chat_type='private', content_types=types.ContentTypes.ANY)
 async def add_request(message: types.Message, state: FSMContext):
     if message.forward_from_chat and message.forward_from_chat.type == 'channel':
@@ -18,9 +16,7 @@ async def add_request(message: types.Message, state: FSMContext):
                 async with state.proxy() as data: 
                     data['channel_id'] = message.forward_from_chat.id
                     data['username'] = message.forward_from_chat.username
-
                 await message.answer('Пришлите количество просмотров, с которого бот должен начать проверки')
-
                 await AddChannel.good_views.set()
             else:
                 await message.answer(f'Сначала добавьте бота в админы @{bot_info.username}')
@@ -31,7 +27,6 @@ async def add_request(message: types.Message, state: FSMContext):
     else:
         await message.answer('Нужно переслать пост с канала!')
         await state.finish()
-
 
 
 @dp.message_handler(IsAdmin(), state=AddChannel.good_views, chat_type='private')
@@ -47,29 +42,23 @@ async def add_request(message: types.Message, state: FSMContext):
         await message.answer('Это не число')
 
 
-
-
 @dp.message_handler(IsAdmin(), state=AddChannel.view_check, chat_type='private')
 async def add_request(message: types.Message, state: FSMContext):
     if message.text.isdigit()and int(message.text) >= 10:
         async with state.proxy() as data:
             data['view_check'] = int(message.text)
-
         await message.answer('Пришлите частоту проверки поста в секундах')
-
         await AddChannel.time_check.set()
     else:
         await message.answer('Число должно быть больше >=10')
 
 
-        
 @dp.message_handler(IsAdmin(), state=AddChannel.time_check, chat_type='private')
 async def add_request(message: types.Message, state: FSMContext):
     if message.text.isdigit() and int(message.text) >= 10:
         async with state.proxy() as data: 
             data['time_check'] = int(message.text)
             await db.add_channel(**data)
-
         await state.finish()
         await message.answer('Канал успешно добавлен')
     else:
