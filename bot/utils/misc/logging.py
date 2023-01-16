08c4a98609e68d3ac0c0bbf39066f9eb213a6ba6
Michael0l <40627547+Michael0l@users.noreply.github.com>
2023-01-16 22:05:55 +0300

    Update logging.py

--- bot/utils/misc/logging.py
+++ bot/utils/misc/logging.py
@@ -4,5 +4,4 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
                     filename='logging.log',
                     filemode='a',
                     level=logging.WARNING,
-                    # level=logging.DEBUG,  # Можно заменить на другой уровень логгирования.
                     )
