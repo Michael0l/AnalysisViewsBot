f0122b837fcf6ff9e9ca4ff446b041d450fbc1bc
Michael0l <40627547+Michael0l@users.noreply.github.com>
2023-01-16 22:07:26 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,8 +2,7 @@ from aiogram.utils import executor
 from aiogram import Dispatcher
 
 from .utils.loader import dp, db, sched
-
-from bot.utils.set_bot_commands import set_default_commands
+from .utils.set_bot_commands import set_default_commands
 from bot import handlers
 
 
@@ -12,7 +11,6 @@ async def on_start_up(dp: Dispatcher):
     await db.create_tables()
     await sched.run_channel_viewing()
     await sched.uploading_cheating_post_to_timer_from_db()
-
 
 def start_bot():
     executor.start_polling(dp, skip_updates=True, on_startup=on_start_up)
